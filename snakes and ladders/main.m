86dadca9cd7cd3580b4d31bae5e4f457158a4ed4
carmen-c <carmen.csmwc@gmail.com>
2016-11-05 17:53:20 -0700

    Initial Commit

---
+++ snakes and ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  snakes and ladders
+//
+//  Created by carmen cheng on 2016-11-05.
+//  Copyright © 2016 carmen cheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
